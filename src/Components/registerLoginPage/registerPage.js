16d27eaaea27b2a85257df8c52ac651de6d53fd7
DESKTOP-378FDDD\Markus <kosyk.markian@gmail.com>
2023-04-28 13:53:49 +0300

    new changes

--- src/Components/registerLoginPage/registerPage.js
+++ src/Components/registerLoginPage/registerPage.js
@@ -11,11 +11,48 @@ function RegisterPage() {
                 <div className={styles.registerForm}>
                     <span className={styles.subTitleForm}>Full Name</span>
                     <input placeholder='Name...' />
+                    <hr />
+                </div>
+                <div className={styles.registerForm}>
+                    <span className={styles.subTitleForm}>Email</span>
+                    <input placeholder='Email...' />
+                    <hr />
+                </div>
+                <div className={styles.registerForm}>
+                    <span className={styles.subTitleForm}>Password</span>
+                    <input placeholder='*******' type='password' />
+                    <hr />
+                </div>
+                <div className={styles.registerForm}>
+                    <span className={styles.subTitleForm}>Repeat password</span>
+                    <input placeholder='*******' type='password' />
+                    <hr />
+                </div>
+                <div className={styles.registerForm}>
+
+                    <span className={styles.subTitleForm}>Rieltor or Client</span>
+                    <div className={styles.roleChoice}>
+                        <div className={styles.Proffesion}>
+                            <input type='radio' value='Client' name='user' checked className={styles.radioBtn} />
+                            <label htmlFor='Client'>Client</label>
+                        </div>
+                        <div className={styles.Proffesion}>
+                            <input type='radio' value='Rieltor' name='user' className={styles.radioBtn} />
+                            <label htmlFor='Rieltor'>Rieltor</label>
+                        </div>
+                    </div>
+                    <hr />
+                </div>
+                <div className={styles.checkBoxWrapper}>
+                    <input type='checkbox' value='checkbox' name='checkbox for accepting rules' className={styles.checkBox} />
+                    <div className={styles.labelWrapper}>
+                        <label htmlFor='checkbox' className={styles.label}>I agree to the Terms of user</label>
+                    </div>
                 </div>
             </div>
             <div className={styles.resgiterButtons}>
-                <button>Sign up</button>
-                <NavLink to='/login'><button>Sign in</button></NavLink>
+                <button className={styles.signUpBtn}>Sign up</button>
+                <NavLink to='/login' style={{ textDecoration: 'none', textColor: 'black' }} activeStyle={{ color: 'black' }}><span>Sign in &#8594;</span></NavLink>
             </div>
         </div>
     )
